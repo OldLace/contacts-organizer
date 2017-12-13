8e38ca3aadcd0cd76018a4facfc0582352af1fd8
Paul Gelot <pgelot@gmail.com>
2017-12-13 04:00:37 -0500

    CRUD working

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,16 @@
 class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
   def show
     @contact = Contact.find(params[:id])
   end
 
-  def index
-    @contacts = Contact.all
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.destroy
+    redirect_to contacts_path, notice: "Contact information for #{contact.first_name} has been deleted!!!"
   end
 
   # def show
@@ -18,15 +24,33 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
-      flash[:success] = "New Contact Added!!"
-      redirect_to contacts_path
+      redirect_to contacts_path, notice: "New Contact Added!!"
     else
-      render 'new'
+      render :new
     end
   end
 
-  private
+  def edit
+    @contact = Contact.find(params[:id])
+  end
 
+  def update
+    @contact = Contact.find(params[:id])
+
+    if @contact.update_attributes(contact_params)
+      redirect_to contact_path(@contact), notice: "Contact Updated!"
+    else
+      redirect_to contact_path(@contact), notice: "Update Failed!!!"
+
+      #   @errors = @contact.errors.full_messages
+    #   render :edit
+    end
+  end
+
+
+
+  private
+# allows users to send the specified data, but nothing more
  def contact_params
    params.require(:contact).permit(:first_name, :last_name, :phone, :email, :address)
  end
